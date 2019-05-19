d1ca5a38ddb4debbe4c60d8be688c04b5daf01cc
conniepope <conniepope77@gmail.com>
2019-05-19 18:10:33 -0400

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+//VARIABLES
+var timer;
+// questions with answer options (as radio buttons?) with only one being able to be selected
+var questions = {
+    q1: {
+        q: "Why is the sky blue?",
+        a1: "Because",
+        a2: "I don't know",
+        a3: "Someone painted it that way",
+        a4: "Science",
+    },
+    q2: {
+        q:   ,
+        a1:  ,
+        a2:  ,
+        a3:  ,
+        a4:  ,
+    }
+}
+
+//RESET  - at start:
+
+
+// timer will count down from 20 seconds
+
+//when timer reachs 0,
+
+//next question with answer options displays
