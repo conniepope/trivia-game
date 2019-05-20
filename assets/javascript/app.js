8cb0f337deed81632f24c7e7e6e01a3105111363
conniepope <conniepope77@gmail.com>
2019-05-19 20:25:12 -0400

    html and pseudocode set up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,73 @@
 //VARIABLES
 var timer;
+var correctAnswers;
+var incorrectAnswers;
 // questions with answer options (as radio buttons?) with only one being able to be selected
 var questions = {
     q1: {
-        q: "Why is the sky blue?",
-        a1: "Because",
-        a2: "I don't know",
-        a3: "Someone painted it that way",
-        a4: "Science",
+        q: "This artist is sometimes credited with the inventions of the parachute, helicopter, and tank.",
+        a1: "Salvador Dali",
+        a2: "Pablo Picasso",
+        a3: "Leonardo da Vinci",
+        a4: "Vincent Van Gogh",
     },
     q2: {
-        q:   ,
-        a1:  ,
-        a2:  ,
-        a3:  ,
-        a4:  ,
+        q: "Who painted 'The Persistence of Memory', an urrealistic image of soft, melting pocket watches?"  ,
+        a1: "Rembrandt",
+        a2: "Salvador Dali",
+        a3: "Andy Warhol",
+        a4: "Michelangelo" ,
     }
 }
 
-//RESET  - at start:
+// START SCREEN
+
+    // show start button
+
+    correctAnswers = 0;
+    incorrectAnswers = 0;
 
 
-// timer will count down from 20 seconds
+    //once the start button is clicked  
 
-//when timer reachs 0,
+// GAME PLAY
 
-//next question with answer options displays
+    // hide start button
+
+    // QUESTION SCREEN/TIMER
+
+        // display timer & first question w/answers
+
+        // timer will count down from 20 seconds
+
+        //when timer reachs 0,
+
+    // ANSWER SCREEN
+
+        //if correct answer,
+            correctAnswers++;
+            // display congrats for 5 seconds
+            // and display correct answer,
+        // if incorrect answer,
+            incorrectAnswers++;
+            //display wrong for 5 seconds
+            // and display correct answer,
+
+        // if no answer,
+            //display 'time has run out'
+            incorrectAnswers++
+            // display correct answer
+
+    //next question with answer options displays after correct answer is displayed for 5 seconds
+
+    // SCORING SCREEN
+
+        //display finished text
+        
+        //display correctAnswers
+
+        //display incorrectAnswers
+
+        //display startOver button
+
+        // when startOver button is pressed, go back to the start screen
