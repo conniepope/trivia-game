8025aedac73c7b5fe060bfb01e9b8c79a925bf18
conniepope <conniepope77@gmail.com>
2019-05-21 20:28:26 -0400

    radio buttons created and functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,120 @@
-//VARIABLES
-var timer;
-var correctAnswers;
-var incorrectAnswers;
-// questions with answer options (as radio buttons?) with only one being able to be selected
-var questions = [
-    {
-        q: "This artist is sometimes credited with the inventions of the parachute, helicopter, and tank.",
-        options: ["Salvador Dali", "Pablo Picasso", "Leonardo da Vinci", "Vincent Van Gogh"],
-        answer: 2
-    },
-    {
-        q: "Who painted 'The Persistence of Memory', an urrealistic image of soft, melting pocket watches?"  ,
-        options: ["Rembrandt", "Salvador Dali", "Andy Warhol", "Michelangelo"],
-        answer: 1
-    },
-];
+$(document).ready(function(){
 
-// START SCREEN
-function reset() {
-// show start button
-    $("#start").show();
-    $("#start-over").hide();
+    //VARIABLES
+    var timer;
+    var correctAnswers;
+    var incorrectAnswers;
+    // questions with answer options (as radio buttons?) with only one being able to be selected
+    var questions = [
+        {
+            q: "This artist is sometimes credited with the inventions of the parachute, helicopter, and tank.",
+            options: ["Salvador Dali", "Pablo Picasso", "Leonardo da Vinci", "Vincent Van Gogh"],
+            answer: 2
+        },
+        {
+            q: "Who painted 'The Persistence of Memory', an urrealistic image of soft, melting pocket watches?" ,
+            options: ["Rembrandt", "Salvador Dali", "Andy Warhol", "Michelangelo"],
+            answer: 1
+        },
+        {
+            q: "This artist is known for co-founding the Cubist movement, the invention of constructed sculpture,the co-invention of collage.",
+            options: ["Pablo Picasso", "Leonardo da Vinci", "Claude Monet", "Diego Velazquez"],
+            answer: 0
+        }
+    ];
 
-    correctAnswers = 0;
-    incorrectAnswers = 0;
+    // START SCREEN
+    function reset() {
+    // show start button
+        $("#start").show();
+        $("#start-over").hide();
 
-}
-//once the start button is clicked  
-$("#start").on("click", function() {
-    playGame();
-})
-
-// GAME PLAY
-function playGame () {
-    // hide start button
-    $("#start").hide();
-    $("#start-over").hide();
-    
-    // QUESTION SCREEN/TIMER
-
-    // display timer & first question w/answers
-    timer = 20;
-    $(".timer").text(timer);
-    $(".question").html(questions[0].q);
-    console.log(questions[0].q)
-    console.log(questions[0].options);
-    for (var i = 0; i < questions[0].options.length; i++) {
-        $(".answers").html(questions[0].options)
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        console.log ("this is working");
+        //once the start button is clicked  
+        $("#start").on("click", function() {
+            playGame();
+        })
     }
 
 
-        // timer will count down from 20 seconds
+    // GAME PLAY
+    function playGame () {
+        // hide start button
+        $("#start").hide();
+        $("#start-over").hide();
+        
+        // QUESTION SCREEN/TIMER
 
-        //when timer reachs 0,
+        // display timer & first question w/answers
+        timer = 20;
+        $(".timer").text(timer);
+        //timer starts countdown right away
 
-    // ANSWER SCREEN
+        // for (var i = 0; i < questions.length; i++) {
+            $(".question").html(questions[0].q);
+            console.log(questions[0].q)
 
-        //if correct answer,
-            correctAnswers++;
-            // display congrats for 5 seconds
-            // and display correct answer,
-        // if incorrect answer,
-            incorrectAnswers++;
-            //display wrong for 5 seconds
-            // and display correct answer,
+            var answers = $(".answers");
+            answers.html('');
+            for (var i = 0; i < questions[0].options.length; i++) {
+                answers.append('<label><input type="radio" name="options" value="' + questions[0].options[i] + '"/> ' + questions[0].options[i] + '</label><br>');
+            //create radio buttons for answers
+            // $(".answers").append("<button> This is a button.</button>")
 
-        // if no answer,
-            //display 'time has run out'
-            incorrectAnswers++
-            // display correct answer
+            // $(".answers").html(answers)
+            console.log(answers)
+            }
 
-    //next question with answer options displays after correct answer is displayed for 5 seconds
-} 
-    // SCORING SCREEN
-function scoring () {
-        //display finished text
-        $(".how-you-did").text("Are you an art aficionado?")    
-        //display correctAnswers
-        $(".correct-answer-score").text(correctAnswers)
-        //display incorrectAnswers
-        $(".incorrect-answer-score").text(incorrectAnswers)
-        //display startOver button
-        $("#start-over").show()
-        // when startOver button is pressed, go back to the start screen
-        $("#start-over").on("click", function() {
-            reset();
-        });
-    }        
+        // }
+
+
+            // timer will count down from 20 seconds
+
+            //when timer reachs 0,
+
+        // ANSWER SCREEN
+
+            //compare user answer with correct answer
+            $("input[type=radio][name=answer]:checked" ).val();
+
+
+            //if correct answer,
+                correctAnswers++;
+                // display congrats for 5 seconds
+                // and display correct answer,
+            // if incorrect answer,
+                incorrectAnswers++;
+                //display wrong for 5 seconds
+                // and display correct answer,
+
+            // if no answer,
+                //display 'time has run out'
+                incorrectAnswers++
+                // display correct answer
+
+        //next question with answer options displays after correct answer is displayed for 5 seconds
+
+        // timer is reset to 20 seconds
+    } 
+        // SCORING SCREEN
+    function scoring () {
+            //display finished text
+            $(".how-you-did").text("Are you an art aficionado?")    
+            //display correctAnswers
+            $(".correct-answer-score").text(correctAnswers)
+            //display incorrectAnswers
+            $(".incorrect-answer-score").text(incorrectAnswers)
+            //display startOver button
+            $("#start-over").show()
+            // when startOver button is pressed, go back to the start screen
+            $("#start-over").on("click", function() {
+                reset();
+            });
+    };
+
+    reset() 
+
+})  
+    
